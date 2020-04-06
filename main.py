916fbbda54af71e30cb466bc08761bd1e7bb07fd
Rod <36175323+524248@users.noreply.github.com>
2020-04-05 22:27:02 -0600

    Added comment
    Test of editing file on gitHub repo instead of local repo

--- main.py
+++ main.py
@@ -4,5 +4,6 @@ m = MainClass()
 result = m.func_1(1, 2)
 print('main: result={}'.format(result))
 
+# get buffer and print it for testing or review
 pB = m.loadBuf()
 print('main: pB={}'.format(pB))
